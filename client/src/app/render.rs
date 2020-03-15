2c716ccad00eae218f0ddac17138f4923e524871
memoryleak47 <memoryleak47@protonmail.ch>
2020-03-15 17:39:07 +0100

    made Vec2f even more awesome

--- client/src/app/render.rs
+++ client/src/app/render.rs
@@ -1,6 +1,6 @@
 use sfml::graphics::Color;
 
-use rustarena_lib::geom::{Vec2f, Circle};
+use rustarena_lib::geom::Circle;
 
 use crate::app::App;
 use crate::app::window::Window;
@@ -21,7 +21,7 @@ impl App {
 
 		// health bars
 		for p in self.world.players.iter() {
-			self.window.draw_rect(p.center() - Vec2f::new(0., p.radius() * 1.2), (p.health as f32 / 100. * 2. * p.radius(), 0.004), Color::rgb(244, 0, 0));
+			self.window.draw_rect(p.center() - (0., p.radius() * 1.2), (p.health as f32 / 100. * 2. * p.radius(), 0.004), Color::rgb(244, 0, 0));
 		}
 	}
 }
--- lib/src/geom/vec.rs
+++ lib/src/geom/vec.rs
@@ -1,181 +1,28 @@
-use std::ops::{Add, AddAssign, Sub, Mul, Div};
+use std::ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, DivAssign};
 use std::fmt::{Display, Debug, Error, Formatter};
-use std::hash::{Hash, Hasher};
 use std::convert::From;
 
+use crate::WORLD_SIZE;
+
 #[derive(Serialize, Deserialize)]
-#[derive(Default)]
-pub struct Vec2t<T> {
-	pub x: T,
-	pub y: T,
+#[derive(Default, PartialEq, Clone, Copy)]
+pub struct Vec2f {
+	pub x: f32,
+	pub y: f32,
 }
 
-#[allow(dead_code)]
-pub type Vec2f = Vec2t<f32>;
-
-impl<T> Vec2t<T> {
-	pub const fn new(x: T, y: T) -> Vec2t<T> {
-		Vec2t { x, y }
+impl Vec2f {
+	pub const fn new(x: f32, y: f32) -> Vec2f {
+		Vec2f { x, y }
 	}
-}
 
-impl<T: Copy> Vec2t<T> {
-	pub fn with(a: T) -> Vec2t<T> {
-		Vec2t {
-			x: a,
-			y: a,
-		}
-	}
-}
-
-impl<T> Add<Vec2t<T>> for Vec2t<T> where T: Add<Output=T> {
-	type Output = Vec2t<T>;
-
-	fn add(self, other: Vec2t<T>) -> Vec2t<T> {
-		Vec2t::new (
-			self.x + other.x,
-			self.y + other.y,
-		)
-	}
-}
-
-impl<T> AddAssign for Vec2t<T> where T: AddAssign {
-	fn add_assign(&mut self, other: Vec2t<T>) {
-		self.x += other.x;
-		self.y += other.y;
-	}
-}
-
-impl<T> Add<T> for Vec2t<T> where T: Add<Output=T> + Copy {
-	type Output = Vec2t<T>;
-
-	fn add(self, other: T) -> Vec2t<T> {
-		Vec2t::new (
-			self.x + other,
-			self.y + other,
-		)
-	}
-}
-
-impl<T> Sub<Vec2t<T>> for Vec2t<T> where T: Sub<Output=T> {
-	type Output = Vec2t<T>;
-
-	fn sub(self, other: Vec2t<T>) -> Vec2t<T> {
-		Vec2t::new (
-			self.x - other.x,
-			self.y - other.y,
-		)
-	}
-}
-
-impl<T> Sub<T> for Vec2t<T> where T: Sub<Output=T> + Copy {
-	type Output = Vec2t<T>;
-
-	fn sub(self, other: T) -> Vec2t<T> {
-		Vec2t::new (
-			self.x - other,
-			self.y - other,
-		)
-	}
-}
-
-impl<T> Mul<Vec2t<T>> for Vec2t<T> where T: Mul<Output=T> {
-	type Output = Vec2t<T>;
-
-	fn mul(self, other: Vec2t<T>) -> Vec2t<T> {
-		Vec2t::new (
-			self.x * other.x,
-			self.y * other.y,
-		)
-	}
-}
-
-impl<T> Mul<T> for Vec2t<T> where T: Mul<Output=T> + Copy {
-	type Output = Vec2t<T>;
-
-	fn mul(self, other: T) -> Vec2t<T> {
-		Vec2t::new (
-			self.x * other,
-			self.y * other,
-		)
-	}
-}
-
-impl<T> Div<Vec2t<T>> for Vec2t<T> where T: Div<Output=T> {
-	type Output = Vec2t<T>;
-
-	fn div(self, other: Vec2t<T>) -> Vec2t<T> {
-		Vec2t::new (
-			self.x / other.x,
-			self.y / other.y,
-		)
-	}
-}
-
-impl<T> Div<T> for Vec2t<T> where T: Div<Output=T> + Copy {
-	type Output = Vec2t<T>;
-
-	fn div(self, other: T) -> Vec2t<T> {
-		Vec2t::new (
-			self.x / other,
-			self.y / other,
-		)
-	}
-}
-
-impl<T> Hash for Vec2t<T> where T: Hash {
-	fn hash<H: Hasher>(&self, h: &mut H) {
-		self.x.hash(h);
-		self.y.hash(h);
-		h.finish();
-	}
-}
-
-impl<T: PartialEq> PartialEq for Vec2t<T> {
-	fn eq(&self, rhs: &Self) -> bool {
-		(self.x == rhs.x) && (self.y == rhs.y)
-	}
-}
-
-impl<T: Eq> Eq for Vec2t<T> {}
-
-impl<T> Vec2t<T> {
-	pub fn map<U, F: Fn(T) -> U>(self, f: F) -> Vec2t<U> {
-		Vec2t::new(
+	pub fn map<F: Fn(f32) -> f32>(self, f: F) -> Vec2f {
+		Vec2f::new(
 			f(self.x),
 			f(self.y),
 		)
 	}
-}
 
-impl<T: Clone> Clone for Vec2t<T> {
-	fn clone(&self) -> Self {
-		Vec2t::new(
-			self.x.clone(),
-			self.y.clone(),
-		)
-	}
-}
-
-impl<T: Copy> Copy for Vec2t<T> { }
-
-impl<T: Display> Display for Vec2t<T> {
-	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
-		let s = format!("Vec2t({}, {})", self.x, self.y);
-		fmt.write_str(&*s)
-	}
-}
-
-impl<T: Debug> Debug for Vec2t<T> {
-	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
-		let s = format!("Vec2t({:?}, {:?})", self.x, self.y);
-		fmt.write_str(&*s)
-	}
-}
-
-use crate::WORLD_SIZE;
-
-impl Vec2f {
 	pub fn distance(self, other: Vec2f) -> f32 {
 		(self - other).magnitude()
 	}
@@ -210,8 +57,106 @@ impl Vec2f {
 	}
 }
 
-impl<T> From<(T, T)> for Vec2t<T> {
-	fn from(item: (T, T)) -> Vec2t<T> {
-		Vec2t::new(item.0, item.1)
+impl From<(f32, f32)> for Vec2f {
+	fn from(item: (f32, f32)) -> Vec2f {
+		Vec2f::new(item.0, item.1)
+	}
+}
+
+impl From<f32> for Vec2f {
+	fn from(item: f32) -> Vec2f {
+		Vec2f::new(item, item)
+	}
+}
+
+impl Display for Vec2f {
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+		let s = format!("Vec2f({}, {})", self.x, self.y);
+		fmt.write_str(&*s)
+	}
+}
+
+impl Debug for Vec2f {
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+		let s = format!("Vec2f({:?}, {:?})", self.x, self.y);
+		fmt.write_str(&*s)
+	}
+}
+
+// operator implementations:
+
+// add
+impl<T: Into<Vec2f>> Add<T> for Vec2f {
+	type Output = Vec2f;
+
+	fn add(self, other: T) -> Vec2f {
+		let other = other.into();
+		Vec2f::new (
+			self.x + other.x,
+			self.y + other.y,
+		)
+	}
+}
+
+impl<T: Into<Vec2f>> AddAssign<T> for Vec2f {
+	fn add_assign(&mut self, other: T) {
+		*self = *self + other.into();
+	}
+}
+
+// sub
+impl<T: Into<Vec2f>> Sub<T> for Vec2f {
+	type Output = Vec2f;
+
+	fn sub(self, other: T) -> Vec2f {
+		let other = other.into();
+		Vec2f::new (
+			self.x - other.x,
+			self.y - other.y,
+		)
+	}
+}
+
+impl<T: Into<Vec2f>> SubAssign<T> for Vec2f {
+	fn sub_assign(&mut self, other: T) {
+		*self = *self - other.into();
+	}
+}
+
+// mul
+impl<T: Into<Vec2f>> Mul<T> for Vec2f {
+	type Output = Vec2f;
+
+	fn mul(self, other: T) -> Vec2f {
+		let other = other.into();
+		Vec2f::new (
+			self.x * other.x,
+			self.y * other.y,
+		)
+	}
+}
+
+impl<T: Into<Vec2f>> MulAssign<T> for Vec2f {
+	fn mul_assign(&mut self, other: T) {
+		*self = *self * other.into();
+	}
+}
+
+// div
+impl<T: Into<Vec2f>> Div<T> for Vec2f {
+	type Output = Vec2f;
+
+	fn div(self, other: T) -> Vec2f {
+		let other = other.into();
+		Vec2f::new (
+			self.x / other.x,
+			self.y / other.y,
+		)
+	}
+}
+
+impl<T: Into<Vec2f>> DivAssign<T> for Vec2f {
+	fn div_assign(&mut self, other: T) {
+		*self = *self / other.into();
 	}
 }
