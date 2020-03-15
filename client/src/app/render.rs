04fe39f45390c540cce9083af8fde565084d07dd
memoryleak47 <memoryleak47@protonmail.ch>
2020-03-15 16:33:31 +0100

    ...

--- client/src/app/render.rs
+++ client/src/app/render.rs
@@ -7,22 +7,20 @@ use crate::app::window::Window;
 
 impl App {
 	pub fn render(&mut self) {
-		let pl = self.world.players.clone(); // TODO fix clone
-
 		// player bodies
-		for p in pl.iter() {
+		for p in self.world.players.iter() {
 			self.window.draw_circle(p, Color::rgb(100, 100, 0));
 		}
 
 		// q_skill rendering
-		for p in pl.iter() {
+		for p in self.world.players.iter() {
 			if let Some(ref b) = p.q_skill {
 				self.window.draw_circle(b, Color::rgb(0, 0, 80));
 			}
 		}
 
 		// health bars
-		for p in pl.iter() {
+		for p in self.world.players.iter() {
 			self.window.draw_rect(p.center() - Vec2f::new(0., p.radius() * 1.2), (p.health as f32 / 100. * 2. * p.radius(), 0.004), Color::rgb(244, 0, 0));
 		}
 	}
