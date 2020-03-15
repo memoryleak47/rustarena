5a2660abc311e95a690b57c4596d7d72d5de5314
memoryleak47 <memoryleak47@protonmail.ch>
2020-03-15 05:42:38 +0100

    added health bar rendering

--- client/src/app/render.rs
+++ client/src/app/render.rs
@@ -4,14 +4,26 @@ use sfml::system::Vector2f;
 use sfml::graphics::{RenderTarget, RectangleShape, Shape, Color, Transformable};
 
 impl App {
+	fn draw_rect(&mut self, pos: (i64, i64), size: (i64, i64), c: Color) {
+		let mut s = RectangleShape::new();
+		let v = Vector2f::new(pos.0 as f32, pos.1 as f32);
+		s.set_position(v);
+		s.set_size(Vector2f::new(size.0 as f32, size.1 as f32));
+		s.set_fill_color(c);
+		self.window.draw(&s);
+	}
+
 	pub fn render(&mut self) {
-		for p in self.state.w.players.iter() {
-			let mut s = RectangleShape::new();
-			let v = Vector2f::new(p.x as f32, p.y as f32);
-			s.set_position(v);
-			s.set_size(Vector2f::new(20., 20.));
-			s.set_fill_color(Color::rgb(100, 100, 0));
-			self.window.draw(&s);
+		let pl = self.state.w.players.clone(); // TODO fix clone
+
+		// player bodies
+		for p in pl.iter() {
+			self.draw_rect((p.x, p.y), (20, 20), Color::rgb(100, 100, 0));
+		}
+
+		// health bars
+		for p in pl.iter() {
+			self.draw_rect((p.x, p.y - 12), ((p.health / 5) as i64, 2), Color::rgb(244, 0, 0));
 		}
 	}
 }
--- lib/src/world.rs
+++ lib/src/world.rs
@@ -1,7 +1,19 @@
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Player {
 	pub x: i64,
-	pub y: i64
+	pub y: i64,
+	pub health: u32,
+}
+
+impl Player {
+	#[allow(clippy::new_without_default)]
+	fn new() -> Player {
+		Player {
+			x: 0,
+			y: 0,
+			health: 100,
+		}
+	}
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -13,7 +25,7 @@ impl World {
 	#[allow(clippy::new_without_default)]
 	pub fn new() -> World {
 		World {
-			players: [Player { x: 50, y: 20 }, Player { x: 500, y: 20 }]
+			players: [Player::new(), Player::new()],
 		}
 	}
 }
